e04e7c3df8b3b35253796e772632def8dd52bddb
jonschwadron <jonschwadron@gmail.com>
2017-06-09 14:36:27 -0500

    route changes

--- client/main.js
+++ client/main.js
@@ -5,72 +5,73 @@ import '../imports/ui/jquery-ui.min.css';
 import '../imports/api/bootstrap.min.js';
 import '../imports/api/jquery-ui.min.js';
 import '../imports/api/jquery.ui.touch-punch.min.js';
+import '../imports/api/jquery.simpleWeather.min.js';
 import '../imports/api/typed.js';
 import '../imports/ui/airbnb.js';
 import './main.html';
 
 Router.route('/', function () {
-  this.render('Resume');
-});
-
-Router.route('/work', function () {
-  this.render('Work');
-});
-
-Router.route('/darth-plagueis', function () {
-  this.render('DarthPlagueis');
-});
-
-Router.route('/pokebox-css', function () {
-  this.render('PokeboxCSS');
-});
-
-Router.route('/happiness-calculator', function () {
-  this.render('HappinessCalculator');
-});
-
-Router.route('/timeline', function () {
-  this.render('Timeline');
+	this.render('Overview');
 });
 
 Router.route('/resume', function () {
-  this.render('Resume');
+	this.render('Resume');
+});
+
+Router.route('/work', function () {
+	this.render('Work');
+});
+
+Router.route('/darth-plagueis', function () {
+	this.render('DarthPlagueis');
+});
+
+Router.route('/pokebox-css', function () {
+	this.render('PokeboxCSS');
+});
+
+Router.route('/happiness-calculator', function () {
+	this.render('HappinessCalculator');
+});
+
+Router.route('/timeline', function () {
+	this.render('Timeline');
 });
 
 Router.route('/grocery-app', function () {
-  this.render('GroceryApp');
+	this.render('GroceryApp');
 });
 
 Router.route('/fingr', function () {
-  this.render('Fingr');
+	this.render('Fingr');
 });
 
 Router.route('/fsc-virtual-id', function () {
-  this.render('FSCVirtualID');
+	this.render('FSCVirtualID');
 });
 
 Router.route('/ansible', function () {
-  this.render('Ansible');
+	this.render('Ansible');
 });
 
 Router.route('/chef', function () {
-  this.render('Chef');
+	this.render('Chef');
 });
 
 Router.route('/cool', function () {
-  this.render('Cool');
+	this.render('Cool');
 });
 
 Router.route('/contact', function () {
-  this.render('Contact');
+	this.render('Contact');
 });
 
 Router.route('/airbnb', function () {
-  this.render('Airbnb');
+	this.render('Airbnb');
 });
 
 Router.route('/django-rating-system', function () {
-  this.render('DjangoRatingSystem');
+	this.render('DjangoRatingSystem');
 });
 
 
@@ -78,132 +79,179 @@ Router.route('/django-rating-system', function () {
 
 if (Meteor.isClient) {
 
+	Template.Overview.onRendered(function () {
+		new Clipboard('.btn2');
+
+		// Docs at http://simpleweatherjs.com
+
+		/* Does your browser support geolocation? */
+		if ("geolocation" in navigator) {
+			$('.js-geolocation').show();
+		} else {
+			$('.js-geolocation').hide();
+		}
+
+		/* Where in the world are you? */
+		$('.js-geolocation').on('click', function () {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				loadWeather(position.coords.latitude + ',' + position.coords.longitude); //load weather using your lat/lng coordinates
+			});
+		});
+
+		/* 
+		* Test Locations
+		* Austin lat/long: 30.2676,-97.74298
+		* Austin WOEID: 2357536
+		*/
+		$(document).ready(function () {
+			loadWeather('Kansas City', ''); //@params location, woeid
+		});
+
+		function loadWeather(location, woeid) {
+			$.simpleWeather({
+				location: location,
+				woeid: woeid,
+				unit: 'f',
+				success: function (weather) {
+					html = '<h2><i class="icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+					html += '<ul><li>' + weather.city + ', ' + weather.region + '</li>';
+
+					$("#weather").html(html);
+				},
+				error: function (error) {
+					$("#weather").html('<p>' + error + '</p>');
+				}
+			});
+		}
+
+	});
+
 	Template.Contact.helpers({
-	  mapOptions: function() {
-	    // Make sure the maps API has loaded
-	    if (GoogleMaps.loaded()) {
-	      // Map initialization options
-	      return {
-	        center: new google.maps.LatLng(39.099726, -94.576629),
-	        zoom: 14,
-			disableDefaultUI: true,
-			draggable: false,
-		    scrollwheel: false,
-		    panControl: false,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-	      };
-	    }
-	  }
+		mapOptions: function () {
+			// Make sure the maps API has loaded
+			if (GoogleMaps.loaded()) {
+				// Map initialization options
+				return {
+					center: new google.maps.LatLng(39.099726, -94.576629),
+					zoom: 14,
+					disableDefaultUI: true,
+					draggable: false,
+					scrollwheel: false,
+					panControl: false,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
+			}
+		}
 	});
 
-	Template.Contact.onCreated(function() {
+	Template.Contact.onCreated(function () {
 		new Clipboard('.btn');
-	  // We can use the `ready` callback to interact with the map API once the map is ready.
-	  GoogleMaps.ready('map', function(map) {
-	    // Add a marker to the map once it's ready
-	    // var marker = new google.maps.Marker({
-	    //   position: map.options.center,
-	    //   map: map.instance
-	    // });
-	  });
+		// We can use the `ready` callback to interact with the map API once the map is ready.
+		GoogleMaps.ready('map', function (map) {
+			// Add a marker to the map once it's ready
+			// var marker = new google.maps.Marker({
+			//   position: map.options.center,
+			//   map: map.instance
+			// });
+		});
 	});
 
-	Template.Contact.onRendered(function(){
-		GoogleMaps.load({key:'AIzaSyBO04ekYQ1HfR856AIU3HLvsljXDIGMF0c'});
+	Template.Contact.onRendered(function () {
+		GoogleMaps.load({ key: 'AIzaSyBO04ekYQ1HfR856AIU3HLvsljXDIGMF0c' });
 	});
 
-	Template.Timeline.onRendered(function(){
-		this.$('#bbytes').hover(function() {
+	Template.Timeline.onRendered(function () {
+		this.$('#bbytes').hover(function () {
 			$('#bbytes-logo').css('transition', 'all 0.5s');
 			$('#bbytes-logo').css('visibility', 'visible');
 			$('#bbytes-logo').css('transform', 'translateX(0px)');
-		}, function() {
+		}, function () {
 			// on mouseout, reset the background colour
 			$('#bbytes-logo').css('visibility', '');
 			$('#bbytes-logo').css('transform', '');
 		});
 
-		this.$('#ga').hover(function() {
+		this.$('#ga').hover(function () {
 			$('#ga-logo').css('transition', 'all 0.5s');
 			$('#ga-logo').css('visibility', 'visible');
 			$('#ga-logo').css('transform', 'translateX(0px)');
-		}, function() {
+		}, function () {
 			$('#ga-logo').css('visibility', '');
 			$('#ga-logo').css('transform', '');
 		});
 
-		this.$('#ciq').hover(function() {
+		this.$('#ciq').hover(function () {
 			$('#ciq-logo').css('transition', 'all 0.5s');
 			$('#ciq-logo').css('visibility', 'visible');
 			$('#ciq-logo').css('transform', 'translateX(0px)');
-		}, function() {
+		}, function () {
 			// on mouseout, reset the background colour
 			$('#ciq-logo').css('visibility', '');
 			$('#ciq-logo').css('transform', '');
 		});
 
-		this.$('#rlabs').hover(function() {
+		this.$('#rlabs').hover(function () {
 			$('#rlabs-logo').css('transition', 'all 0.5s');
 			$('#rlabs-logo').css('visibility', 'visible');
 			$('#rlabs-logo').css('transform', 'translateX(0px)');
-		}, function() {
+		}, function () {
 			// on mouseout, reset the background colour
 			$('#rlabs-logo').css('visibility', '');
 			$('#rlabs-logo').css('transform', '');
 		});
 
-		this.$('#fdale').hover(function() {
+		this.$('#fdale').hover(function () {
 			$('#fdale-logo').css('transition', 'all 0.5s');
 			$('#fdale-logo').css('visibility', 'visible');
 			$('#fdale-logo').css('transform', 'translateX(0px)');
-		}, function() {
+		}, function () {
 			// on mouseout, reset the background colour
 			$('#fdale-logo').css('visibility', '');
 			$('#fdale-logo').css('transform', '');
 		});
 
-		this.$('#freelance').hover(function() {
+		this.$('#freelance').hover(function () {
 			$('#freelance-logo').css('transition', 'all 0.5s');
 			$('#freelance-logo').css('visibility', 'visible');
 			$('#freelance-logo').css('transform', 'translateX(0px)');
-		}, function() {
+		}, function () {
 			// on mouseout, reset the background colour
 			$('#freelance-logo').css('visibility', '');
 			$('#freelance-logo').css('transform', '');
 		});
 
-		this.$('#ritntid').hover(function() {
+		this.$('#ritntid').hover(function () {
 			$('#ritntid-logo').css('transition', 'all 0.5s');
 			$('#ritntid-logo').css('visibility', 'visible');
 			$('#ritntid-logo').css('transform', 'translateX(0px)');
-		}, function() {
+		}, function () {
 			// on mouseout, reset the background colour
 			$('#ritntid-logo').css('visibility', '');
 			$('#ritntid-logo').css('transform', '');
 		});
 
-		this.$('#scboces').hover(function() {
+		this.$('#scboces').hover(function () {
 			$('#scboces-logo').css('transition', 'all 0.5s');
 			$('#scboces-logo').css('visibility', 'visible');
 			$('#scboces-logo').css('transform', 'translateX(0px)');
-		}, function() {
+		}, function () {
 			// on mouseout, reset the background colour
 			$('#scboces-logo').css('visibility', '');
 			$('#scboces-logo').css('transform', '');
 		});
 
-		this.$('#ouboces').hover(function() {
+		this.$('#ouboces').hover(function () {
 			$('#ouboces-logo').css('transition', 'all 0.5s');
 			$('#ouboces-logo').css('visibility', 'visible');
 			$('#ouboces-logo').css('transform', 'translateX(0px)');
-		}, function() {
+		}, function () {
 			// on mouseout, reset the background colour
 			$('#ouboces-logo').css('visibility', '');
 			$('#ouboces-logo').css('transform', '');
 		});
 
-		this.$('#timeline li').each(function(index) {
-		    $(this).delay(300*index).fadeIn(1000);
+		this.$('#timeline li').each(function (index) {
+			$(this).delay(300 * index).fadeIn(1000);
 		});
 
 		this.$('#timeEntry1').typed({
@@ -218,7 +266,7 @@ if (Meteor.isClient) {
 			strings: ["New York, NY<br>Joined: September 2015<br>Cohort: Web Development Immersive<br>Experience/skills acquired: SCSS, Node.js, Express.js, Angular, MongoDB, Ruby on Rails<br>"],
 			contentType: 'html',
 			typeSpeed: -25,
-			startDelay: 750*2,
+			startDelay: 750 * 2,
 			showCursor: false
 		});
 
@@ -226,7 +274,7 @@ if (Meteor.isClient) {
 			strings: ["New York, NY<br>Category: Data Visualizaiton<br>Joined: July 2015<br>Left: August 2015<br>Position: Full Stack Developer<br>Experience/skills: acquired: Django, Angular, PostgreSQL, Git"],
 			contentType: 'html',
 			typeSpeed: -25,
-			startDelay: 750*3,
+			startDelay: 750 * 3,
 			showCursor: false
 		});
 
@@ -234,7 +282,7 @@ if (Meteor.isClient) {
 			strings: ["Boulder, CO<br>Category: 3D Technology<br>Joined: October 2014<br>Left: December 2015<br>Position: Software Engineer<br>Remote: Yes<br>Experience/skills acquired: DevOps, Chef, Ansible"],
 			contentType: 'html',
 			typeSpeed: -25,
-			startDelay: 750*4,
+			startDelay: 750 * 4,
 			showCursor: false
 		});
 
@@ -242,7 +290,7 @@ if (Meteor.isClient) {
 			strings: ["Farmingdale, NY<br>Graduated: September 2014<br>Major: Computer Programming &amp; Information Systems<br>Degree: Bachelor of Science<br>Experience/skills acquired: C++, Java, Android Development"],
 			contentType: 'html',
 			typeSpeed: -25,
-			startDelay: 750*5,
+			startDelay: 750 * 5,
 			showCursor: false
 		});
 
@@ -250,7 +298,7 @@ if (Meteor.isClient) {
 			strings: ["Orange County, NY<br>Category: Information Technology<br>Joined: March 2008<br>Left: August 2012<br>Experience/skills acquired: Built custom gaming PCs, CPU/GPU overclocking, repaired overheating issues, malwares removal, performance optimizations, data recovery, improved home wireless networks."],
 			contentType: 'html',
 			typeSpeed: -25,
-			startDelay: 750*6,
+			startDelay: 750 * 6,
 			showCursor: false
 		});
 
@@ -258,7 +306,7 @@ if (Meteor.isClient) {
 			strings: ["Rochester, NY<br>Graduated: 2010<br>Major: Applied Computer Technology<br>Degree: Associates of Applied Science<br>Experience/skills acquired: Logic Fundamentals, Electronics, Networking, Security, C++, Web Development"],
 			contentType: 'html',
 			typeSpeed: -25,
-			startDelay: 750*7,
+			startDelay: 750 * 7,
 			showCursor: false
 		});
 
@@ -266,7 +314,7 @@ if (Meteor.isClient) {
 			strings: ["Liberty, NY<br>Category: Education<br>Position: Network Technician (internship)<br>Joined: May 2008<br>Left: August 2008<br>Experience/skills acquired: Desktop setup, Networked printer troubleshooting, Server configuration, Active Directory"],
 			contentType: 'html',
 			typeSpeed: -25,
-			startDelay: 750*8,
+			startDelay: 750 * 8,
 			showCursor: false
 		});
 
@@ -274,18 +322,18 @@ if (Meteor.isClient) {
 			strings: ["Goshen, NY<br>Graduated: June 2003<br>Major: Computer Programming<br>Experience/skills acquired: Visual Basic, C++ <br>  "],
 			contentType: 'html',
 			typeSpeed: -25,
-			startDelay: 750*9,
+			startDelay: 750 * 9,
 			showCursor: false
 		});
 	});
 
 	Template.DarthPlagueis.onRendered(function () {
-		Meteor.setInterval(function(){
+		Meteor.setInterval(function () {
 			// var elem = document.getElementById('robot-message');
 			// elem.scrollTop = elem.scrollHeight;
 			var messageWindow = this.$('#robot-message');
 			var scrollHeight = messageWindow.prop('scrollHeight');
-			messageWindow.stop().animate({scrollTop: scrollHeight}, 550);
+			messageWindow.stop().animate({ scrollTop: scrollHeight }, 550);
 		}, 150);
 
 		this.$('.darth-plagueis-story').typed({
@@ -297,14 +345,14 @@ if (Meteor.isClient) {
 	});
 
 	Template.HappinessCalculator.onRendered(function () {
-		$( "#slider-salary" ).slider({
+		$("#slider-salary").slider({
 			value: 50000,
 			// range: true,
 			min: 1000,
 			max: 500000,
 			// values: [ 50000, 70000 ],
-			slide: function( event, ui ) {
-				$( "#user-salary" ).val( "$" + ui.value );
+			slide: function (event, ui) {
+				$("#user-salary").val("$" + ui.value);
 				// $( "#user-salary" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
 				updateTotal();
 			}
@@ -320,23 +368,23 @@ if (Meteor.isClient) {
 		// 	}
 		// });
 
-		$( "#slider-expense" ).slider({
+		$("#slider-expense").slider({
 			range: true,
 			min: 0,
 			max: 5000,
-			values: [ 750, 1250 ],
-			slide: function( event, ui ) {
-				$( "#user-expense" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+			values: [750, 1250],
+			slide: function (event, ui) {
+				$("#user-expense").val("$" + ui.values[0] + " - $" + ui.values[1]);
 				updateTotal();
 			}
 		});
 
-		$( "#user-salary" ).val( "$" + $( "#slider-salary" ).slider( "value" ) );
-		$( "#user-expense" ).val( "$" + $( "#slider-expense" ).slider( "values", 0 )
-		+ " - $" + $( "#slider-expense" ).slider( "values", 1 ) );
+		$("#user-salary").val("$" + $("#slider-salary").slider("value"));
+		$("#user-expense").val("$" + $("#slider-expense").slider("values", 0)
+			+ " - $" + $("#slider-expense").slider("values", 1));
 
-		$( "#slider-salary" ).draggable();
-		$( "#slider-expense" ).draggable();
+		$("#slider-salary").draggable();
+		$("#slider-expense").draggable();
 
 		function updateTotal() {
 			$salary = $("#slider-salary").slider("value");
@@ -345,7 +393,7 @@ if (Meteor.isClient) {
 
 			//assign tax bracket
 			if ($salary <= 9225) { $taxBracket = 10; }
-			else if ($salary >= 9226 && $salary <= 37450 ) { $taxBracket = 15; }
+			else if ($salary >= 9226 && $salary <= 37450) { $taxBracket = 15; }
 			else if ($salary >= 37451 && $salary <= 90750) { $taxBracket = 25; }
 			else if ($salary >= 90751 && $salary <= 189300) { $taxBracket = 28; }
 			else if ($salary >= 189301 && $salary <= 411500) { $taxBracket = 33; }
@@ -353,7 +401,7 @@ if (Meteor.isClient) {
 			else if ($salary >= 413201) { $taxBracket = 39.6; }
 
 			//factor in tax
-			$salary = $salary - (($taxBracket / 100) * $salary) ;
+			$salary = $salary - (($taxBracket / 100) * $salary);
 
 			//convert to biweekly
 			$minBiWeekly = $salary / 26;
